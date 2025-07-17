0708c57c1b8f17b2a9b9ca1e461bfd4cf436ad25
Schrodinger ZHU Yifan <i@zhuyi.fan>
2025-07-17 17:58:16 -0400

    add basic types

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,1 +1,23 @@
+use ustr::Ustr;
 
+pub mod types;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Path(Box<[Ustr]>);
+
+impl Path {
+    pub fn new(basename: Ustr, prefix: impl IntoIterator<Item = Ustr>) -> Self {
+        let mut all_segments = prefix.into_iter().collect::<Vec<_>>();
+        all_segments.push(basename);
+        Path(all_segments.into_boxed_slice())
+    }
+    pub fn segments(&self) -> &[Ustr] {
+        &self.0
+    }
+    pub fn prefix(&self) -> &[Ustr] {
+        &self.0[..self.0.len() - 1]
+    }
+    pub fn basename(&self) -> Ustr {
+        self.0.last().cloned().expect("Path must have at least one segment")
+    }
+}
