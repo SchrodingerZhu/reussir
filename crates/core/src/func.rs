b51aa8133f5eb04a5bd99acfec76fcb332b7f0d3
Schrodinger ZHU Yifan <i@zhuyi.fan>
2025-07-21 10:24:37 -0400

    prepare statements

---
+++ crates/core/src/func.rs
@@ -0,0 +1,19 @@
+use ustr::Ustr;
+use crate::{Location, Path};
+use crate::types::Type;
+#[derive(Clone, Debug)]
+pub struct Param {
+    pub name: Ustr,
+    pub location: Location,
+    pub ty: Type,
+}
+#[derive(Clone, Debug)]
+pub struct Function {
+    pub path: Path,
+    pub location: Location,
+    pub type_args: Option<Box<[Ustr]>>,
+    pub params: Box<[Param]>,
+    pub return_type: Type,
+    pub region: bool,
+    pub public: bool,
+}
