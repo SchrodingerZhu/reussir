17b760db97cbf087abf0241ca893e46ba54362f2
Schrodinger ZHU Yifan <i@zhuyi.fan>
2025-07-22 23:20:01 -0400

    create builders

--- crates/core/src/ir.rs
+++ crates/core/src/ir.rs
@@ -1,6 +1,12 @@
-use std::cell::Cell;
+use std::{
+    cell::{Cell, RefCell},
+    mem::ManuallyDrop,
+};
 
+use archery::RcK;
 use indexmap::IndexMap;
+use rpds::HashTrieMap;
+use rustc_hash::FxRandomState;
 use ustr::Ustr;
 
 use crate::{
@@ -87,11 +93,49 @@ pub enum OperationKind<'a> {
         target: ValID,
         field: FieldIdentifer,
     },
-    ConstInt(Box<IntegerLiteral>),
-    ConstFloat(Box<FloatLiteral>),
+    ConstInt(&'a IntegerLiteral),
+    ConstFloat(&'a FloatLiteral),
 }
+
+pub type Map<K, V> = HashTrieMap<K, V, RcK, FxRandomState>;
 
 pub struct IRBuilder<'a> {
     arena: &'a bumpalo::Bump,
-    next_val: ValID,
+    next_val: Cell<ValID>,
+    value_types: RefCell<IndexMap<ValID, &'a Type>>,
+    named_values: RefCell<Map<Ustr, ValID>>,
+}
+
+impl<'a> IRBuilder<'a> {
+    pub fn snapshot(&self) -> Snapshot<'_, 'a> {
+        Snapshot {
+            parent: self,
+            value_types: self.value_types.borrow().len(),
+            named_values: ManuallyDrop::new(self.named_values.borrow().clone()),
+        }
+    }
+}
+
+pub struct Snapshot<'p, 'a: 'p> {
+    parent: &'p IRBuilder<'a>,
+    value_types: usize,
+    named_values: ManuallyDrop<Map<Ustr, ValID>>,
+}
+
+impl<'p, 'a> Drop for Snapshot<'p, 'a> {
+    fn drop(&mut self) {
+        self.parent
+            .value_types
+            .borrow_mut()
+            .truncate(self.value_types);
+        self.parent
+            .named_values
+            .replace(unsafe { ManuallyDrop::take(&mut self.named_values) });
+    }
+}
+
+pub struct BlockBuilder<'p, 'a: 'p> {
+    parent: &'p IRBuilder<'a>,
+    operations: Vec<Operation<'a>>,
+    location: Option<Location>,
 }
